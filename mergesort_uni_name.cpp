6a6daed4fd55b56f46d225480cfdd106e1f3110a
leo <ln777764@gmail.com>
2023-05-31 13:56:11 +0800

    succesful sort for register cust

--- mergesort_uni_name.cpp
+++ mergesort_uni_name.cpp
@@ -1,119 +1,169 @@
 #include <iostream>
+#include <chrono>
 #include <fstream>
 #include <sstream>
-#include <algorithm>
+
+
 using namespace std;
+using namespace std::chrono;
 
-// Structure to store university data
-struct University {
-    string name;
-    int students;
-
-    University() {}
-    University(const string& n, int s) : name(n), students(s) {}
-};
-
-// Function to compare universities based on the name
-bool compareUniversities(const University& u1, const University& u2) {
-    return u1.name < u2.name; // Sort in ascending order
+//masukin ke assignment.hpp
+// Function to merge two sorted linked lists based on ArScore
+Institution* mergeByArScore(Institution* left, Institution* right) {
+    if (left == nullptr)
+        return right;
+    if (right == nullptr)
+        return left;
+    
+    Institution* result = nullptr;
+    
+    if (left->ArScore >= right->ArScore) {
+        result = left;
+        result->NextAddress = mergeByArScore(left->NextAddress, right);
+    } else {
+        result = right;
+        result->NextAddress = mergeByArScore(left, right->NextAddress);
+    }
+    
+    return result;
 }
 
-// Function to perform merge operation in merge sort
-void merge(University* arr, int left, int mid, int right) {
-    int leftSize = mid - left + 1;
-    int rightSize = right - mid;
-
-    University* leftArr = new University[leftSize];
-    University* rightArr = new University[rightSize];
-
-    for (int i = 0; i < leftSize; i++) {
-        leftArr[i] = arr[left + i];
+// Function to merge two sorted linked lists based on FsrScore
+Institution* mergeByFsrScore(Institution* left, Institution* right) {
+    if (left == nullptr)
+        return right;
+    if (right == nullptr)
+        return left;
+    
+    Institution* result = nullptr;
+    
+    if (left->FsrScore >= right->FsrScore) {
+        result = left;
+        result->NextAddress = mergeByFsrScore(left->NextAddress, right);
+    } else {
+        result = right;
+        result->NextAddress = mergeByFsrScore(left, right->NextAddress);
     }
-
-    for (int j = 0; j < rightSize; j++) {
-        rightArr[j] = arr[mid + 1 + j];
-    }
-
-    int i = 0, j = 0, k = left;
-
-    while (i < leftSize && j < rightSize) {
-        if (compareUniversities(leftArr[i], rightArr[j])) {
-            arr[k] = leftArr[i];
-            i++;
-        } else {
-            arr[k] = rightArr[j];
-            j++;
-        }
-        k++;
-    }
-
-    while (i < leftSize) {
-        arr[k] = leftArr[i];
-        i++;
-        k++;
-    }
-
-    while (j < rightSize) {
-        arr[k] = rightArr[j];
-        j++;
-        k++;
-    }
-
-    delete[] leftArr;
-    delete[] rightArr;
+    
+    return result;
 }
 
-// Function to perform merge sort
-void mergeSort(University* arr, int left, int right) {
-    if (left < right) {
-        int mid = left + (right - left) / 2;
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid + 1, right);
-        merge(arr, left, mid, right);
+// Function to merge two sorted linked lists based on ErScore
+Institution* mergeByErScore(Institution* left, Institution* right) {
+    if (left == nullptr)
+        return right;
+    if (right == nullptr)
+        return left;
+    
+    Institution* result = nullptr;
+    
+    if (left->ErScore >= right->ErScore) {
+        result = left;
+        result->NextAddress = mergeByErScore(left->NextAddress, right);
+    } else {
+        result = right;
+        result->NextAddress = mergeByErScore(left, right->NextAddress);
     }
+    
+    return result;
 }
 
-int main() {
-    ifstream inputFile("modified_output.txt");
-    if (!inputFile) {
-        cout << "Failed to open file." << endl;
-        return 1;
-    }
+// Function to split the linked list into two halves
+void split(Institution* head, Institution** left, Institution** right) {
+    Institution* slow = head;
+    Institution* fast = head->NextAddress;
 
-    const int size = 1400;
-    University universities[size];
-    int index = 0;
-    string line;
-    getline(inputFile, line); // Skip the header line
+    while (fast != nullptr) {
+        fast = fast->NextAddress;
 
-    while (getline(inputFile, line) && index < size) {
-        istringstream iss(line);
-
-        string rank;
-        getline(iss, rank, ',');
-
-        string name;
-        getline(iss, name, ','); // Extract the institution name from the second field
-
-        // Assuming the format of each line is: Rank,Institution,LocationCode,Location,ArScore,...
-        if (!name.empty()) {
-            int students = 0; // You can add code to extract the number of students from the appropriate field
-
-            universities[index] = University(name, students);
-            index++;
+        if (fast != nullptr) {
+            slow = slow->NextAddress;
+            fast = fast->NextAddress;
         }
     }
 
-    inputFile.close();
+    *left = head;
+    *right = slow->NextAddress;
+    slow->NextAddress = nullptr;
+}
 
-    // Sort the universities based on the name using merge sort
-    mergeSort(universities, 0, index - 1);
 
-    // Print the sorted universities
-    cout << "Sorted Universities by Name:" << endl;
-    for (int i = 0; i < index; i++) {
-        cout << i + 1 << ": " << universities[i].name << endl;
+// Merge sort function for linked list based on ArScore
+void mergeSortByArScore(Institution** headRef) {
+    Institution* head = *headRef;
+    Institution* left = nullptr;
+    Institution* right = nullptr;
+    
+    // Base case: if the list is empty or contains only one node
+    if (head == nullptr || head->NextAddress == nullptr)
+        return;
+    
+    // Split the list into two halves
+    split(head, &left, &right);
+    
+    // Recursively sort the two halves
+    mergeSortByArScore(&left);
+    mergeSortByArScore(&right);
+    
+    // Merge the sorted halves based on ArScore
+    *headRef = mergeByArScore(left, right);
+}
+
+// Merge sort function for linked list based on FsrScore
+void mergeSortByFsrScore(Institution** headRef) {
+    Institution* head = *headRef;
+    Institution* left = nullptr;
+    Institution* right = nullptr;
+    
+    // Base case: if the list is empty or contains only one node
+    if (head == nullptr || head->NextAddress == nullptr)
+        return;
+    
+    // Split the list into two halves
+    split(head, &left, &right);
+    
+    // Recursively sort the two halves
+    mergeSortByFsrScore(&left);
+    mergeSortByFsrScore(&right);
+    
+    // Merge the sorted halves based on FsrScore
+    *headRef = mergeByFsrScore(left, right);
+}
+
+// Merge sort function for linked list based on ErScore
+void mergeSortByErScore(Institution** headRef) {
+    Institution* head = *headRef;
+    Institution* left = nullptr;
+    Institution* right = nullptr;
+    
+    // Base case: if the list is empty or contains only one node
+    if (head == nullptr || head->NextAddress == nullptr)
+        return;
+    
+    // Split the list into two halves
+    split(head, &left, &right);
+    
+    // Recursively sort the two halves
+    mergeSortByErScore(&left);
+    mergeSortByErScore(&right);
+    
+    // Merge the sorted halves based on ErScore
+    *headRef = mergeByErScore(left, right);
+}
+
+// Function to print the linked list with institution name
+void printList(Institution* head) {
+    Institution* temp = head;
+    
+    while (temp != nullptr) {
+        // Print the desired fields from the node
+        cout << "Institution: " << temp->Name << ", ";
+        cout << "ArScore: " << temp->ArScore << ", ";
+        cout << "FsrScore: " << temp->FsrScore << ", ";
+        cout << "ErScore: " << temp->ErScore << endl;
+        
+        temp = temp->NextAddress;
     }
-
-    return 0;
+    
+    cout << endl;
 }
